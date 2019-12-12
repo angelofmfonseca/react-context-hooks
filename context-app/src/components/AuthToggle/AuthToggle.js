51cd45c147675a094f5606a75ea3e355df7457bb
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2019-12-11 22:11:08 -0300

    todos useContext finalizados

--- context-app/src/components/AuthToggle/AuthToggle.js
+++ context-app/src/components/AuthToggle/AuthToggle.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 
 import { AuthContext } from "../../contexts/AuthContext";
 import "./AuthToggle.css";
 
-export default class AuthToggle extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {AuthContext => {
-          const { isAuthenticated, login, logout, toggleAuth } = AuthContext;
-          const auth = isAuthenticated ? login : logout;
-          return (
-            <button
-              className="login"
-              onClick={toggleAuth}
-              style={{ background: auth.bg }}
-            >
-              {isAuthenticated ? "Sair" : "Entrar"}
-            </button>
-          );
-        }}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const AuthToggle = () => {
+  const { isAuthenticated, login, logout, toggleAuth } = useContext(
+    AuthContext
+  );
+  const auth = isAuthenticated ? login : logout;
+
+  return (
+    <button
+      className="login"
+      onClick={toggleAuth}
+      style={{ background: auth.bg }}
+    >
+      {isAuthenticated ? "Sair" : "Entrar"}
+    </button>
+  );
+};
+
+export default AuthToggle;
